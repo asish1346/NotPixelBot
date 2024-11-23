b0d9bc9905d88d180581460864a698be87892a0c
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-11-23 13:20:01 +0530

    Update registrator.py

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,4 +1,3 @@
-import re
 import traceback
 from urllib.parse import urlparse
 
@@ -7,7 +6,7 @@ from pyrogram.client import Client
 from bot.config.config import settings
 from bot.utils.json_manager import JsonManager
 from bot.utils.logger import dev_logger, logger
-from bot.utils.ua_generator import generate_user_agent
+from bot.utils.ua_generator import TelegramUserAgentGenerator
 
 
 async def register_sessions(session_name: str | None = None) -> None:
@@ -27,8 +26,9 @@ async def register_sessions(session_name: str | None = None) -> None:
         raw_proxy = input(
             "Enter proxy in format type://username:password@ip:port (Enter to skip): "
         )
-
-        user_agent = generate_user_agent()
+        
+        user_agent_generator = TelegramUserAgentGenerator()
+        user_agent = user_agent_generator.generate()
 
         session = await get_telegram_client(
             session_name=session_name, user_agent=user_agent, raw_proxy=raw_proxy
@@ -64,14 +64,6 @@ async def get_telegram_client(
         if not settings.API_ID or not settings.API_HASH:
             raise ValueError("API_ID and API_HASH must be set in .env file")
 
-        device_model = re.search(r"Android ([\d.]+); ([\w\s-]+)", user_agent)
-
-        if not device_model:
-            raise ValueError("Invalid user agent")
-
-        system_version = device_model.group(1)
-        device_name = device_model.group(2)
-
         parsed_proxy = urlparse(raw_proxy) if raw_proxy else None
 
         proxy_dict = (
@@ -92,9 +84,6 @@ async def get_telegram_client(
             api_hash=settings.API_HASH,
             proxy=proxy_dict,  # type: ignore
             workdir="sessions",
-            device_model=device_name,
-            system_version=system_version,
-            app_version="Telegram Android 11.4.2",
         )
 
         return telegram_client
