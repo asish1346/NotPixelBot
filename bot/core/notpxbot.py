bda1db788cf50588b77e777a87347be13d971324
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-11-25 08:53:10 +0530

    Update notpxbot.py

--- bot/core/notpxbot.py
+++ bot/core/notpxbot.py
@@ -175,10 +175,10 @@ class NotPXBot:
             country = response_json.get("country", "Not Found")
 
             logger.info(
-                f"{self.session_name} | Proxy connected | IP: {ip} | Country: {country}"
+                f"{self.session_name} |🌐\033[92m Proxy connected \033[0m| IP: {ip} | Country: {country}"
             )
         except Exception:
-            raise Exception(f"{self.session_name} | Proxy error | {proxy}")
+            raise Exception(f"{self.session_name} |\033[31m Proxy error | {proxy}\033[0m")
 
     async def _perform_notpx_actions(
         self, session: aiohttp.ClientSession, telegram_client
@@ -255,7 +255,7 @@ class NotPXBot:
             ):
                 await self._upgrade_boosts(session)
             else:
-                logger.info(f"{self.session_name} | All boosts are maxed out")
+                logger.info(f"{self.session_name} | All Bosts Are Upgraded")
 
         while not self.websocket_manager.is_websocket_connected:
             await asyncio.sleep(2)
@@ -353,11 +353,11 @@ class NotPXBot:
             )
             response.raise_for_status()
 
-            logger.info(f"{self.session_name} | Sent tganalytics event")
+            #logger.info(f"{self.session_name} | Sent tganalytics event")
         except Exception:
             if attempts <= 3:
                 logger.warning(
-                    f"{self.session_name} | Failed to send tganalytics event, retrying in {self.RETRY_DELAY} seconds | Attempts: {attempts}"
+                    f"{self.session_name} | Failed to send tganalytics, retrying in {self.RETRY_DELAY} seconds | Attempts: {attempts}"
                 )
                 await asyncio.sleep(self.RETRY_DELAY)
                 return await self._send_tganalytics_event(
@@ -403,7 +403,7 @@ class NotPXBot:
                 ssl=settings.ENABLE_SSL,
             )
             response.raise_for_status()
-            logger.info(f"{self.session_name} | Plausible event sent")
+            #logger.info(f"{self.session_name} | Plausible event sent")
         except Exception:
             if attempts <= 3:
                 logger.warning(
@@ -458,7 +458,7 @@ class NotPXBot:
             claimed_px = response_json.get("claimed")
 
             logger.info(
-                f"{self.session_name} | Successfully claimed {round(claimed_px, 2)} px"
+                f"{self.session_name} | Successfully claimed \033[95m{round(claimed_px, 2)}\033[0m PX"
             )
 
             plausible_payload = await self._create_plausible_payload(
@@ -473,7 +473,7 @@ class NotPXBot:
 
             if attempts <= 3:
                 logger.warning(
-                    f"{self.session_name} | Failed to claim px, retrying in {self.RETRY_DELAY} seconds | Attempts: {attempts}"
+                       f"{self.session_name} |\033[33m Failed to claim px, retrying in {self.RETRY_DELAY} seconds | Attempts: {attempts}\033[0m"
                 )
                 await asyncio.sleep(self.RETRY_DELAY)
                 return await self._claim_px(session=session, attempts=attempts + 1)
@@ -520,7 +520,7 @@ class NotPXBot:
             )
             response.raise_for_status()
 
-            logger.info(f"{self.session_name} | Successfully set template")
+            logger.info(f"{self.session_name} |\033[32m Successfully set template\033[0m")
 
             plausible_payload = await self._create_plausible_payload(
                 "https://app.notpx.app/"
@@ -621,7 +621,7 @@ class NotPXBot:
                     self._quests_to_complete.append(quest)
 
             logger.info(
-                f"{self.session_name} | Successfully logged in | Balance: {round(self.balance, 2)} | League: {self.league.capitalize()}"
+                f"{self.session_name} | Successfully logged in | Balance:\033[94m {round(self.balance, 2)}\033[0m | League: \033[33m{self.league.capitalize()}\033[0m"
             )
 
         except Exception:
@@ -767,13 +767,13 @@ class NotPXBot:
             if round(new_balance, 2) > round(self.balance, 2):
                 balance_increase = new_balance - self.balance
                 logger.info(
-                    f"{self.session_name} | Successfully painted pixel | +{round(balance_increase, 2)} PX | Charge remaining: {self._charges} | Current balance: {round(new_balance, 2)} PX"
+                    f"{self.session_name} | Successfully painted pixel |\033[32m +{round(balance_increase, 2)}\033[0m PX | Current balance: \033[95m{round(self.balance, 2)}\033[0m PX"
                 )
                 self.balance = new_balance
                 return
 
             logger.warning(
-                f"{self.session_name} | Painted pixel, but balance didn't increase | Current balance: {round(self.balance, 2)} PX | Charge remaining: {self._charges} | Current balance: {round(new_balance, 2)} PX"
+                f"{self.session_name} | Sucessfully Painted pixel |\033[91m +0.0\033[0m PX | Current balance: \033[95m{round(self.balance, 2)}\033[0m PX"
             )
 
     async def _paint_pixels(
@@ -952,7 +952,7 @@ class NotPXBot:
                             task_value
                         ):
                             logger.info(
-                                f"{self.session_name} | Completed task: {task_value}"
+                                f"{self.session_name} | Completed task: \033[95m{task_value}\033[0m"
                             )
                         else:
                             raise Exception(
@@ -1063,7 +1063,7 @@ class NotPXBot:
                 response_json = await response.json()
                 if response_json.get("secretWord").get("success"):
                     logger.info(
-                        f"{self.session_name} | Completed secret word quest | Reward: {response_json.get('secretWord').get('reward')}"
+                        f"{self.session_name} | Completed secret word quest | Reward: \033[92m{response_json.get('secretWord').get('reward')}\033[0m"
                     )
                 else:
                     raise Exception(
