947fac9433f269599fc36415fc90cdd94b07ade0
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-11-28 23:19:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import asyncio
 import sys
 
-from bot.utils.logger import logger
+from bot.utils import logger
 from bot.utils.launcher import process
 
 
 async def main():
-    await process()
+    try:
+        logger.info("<g>Bot started...</g>")
+        await process()
+    except Exception as e:
+        logger.error(f"<r>Error occurred: {e}</r>")
+        sys.exit(1)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logger.warning("Bot stopped")
-        sys.exit()
+        logger.warning("<r>Bot stopped by user...</r>")
+        sys.exit(2)
