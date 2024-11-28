6f487dad2e4ff5674cab630224e8f31b4bcbfd13
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-11-29 00:03:52 +0530

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -7,6 +7,5 @@ logger.remove()
 logger.add(sink=sys.stdout, format="<white>NotPixel</white>"
                                    " | <white>{time:YYYY-MM-DD HH:mm:ss}</white>"
                                    " | <level>{level: <8}</level>"
-                                   " | <cyan>{file}: <b>{line}</b></cyan>"
                                    " - <white><b>{message}</b></white>")
 logger = logger.opt(colors=True)
