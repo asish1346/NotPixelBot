45a73258d3e870226ba16676783f75d86a5af098
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-12-03 11:18:18 +0530

    Tournament Update For R-2

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import asyncio
 import sys
 
-from bot.utils import logger
+from bot.utils.logger import logger
 from bot.utils.launcher import process
 
 
 async def main():
-    try:
-        logger.info("<g>Bot started...</g>")
-        await process()
-    except Exception as e:
-        logger.error(f"<r>Error occurred: {e}</r>")
-        sys.exit(1)
+    await process()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logger.warning("<r>Bot stopped by user...</r>")
-        sys.exit(2)
+        logger.warning("Bot stopped")
+        sys.exit()
